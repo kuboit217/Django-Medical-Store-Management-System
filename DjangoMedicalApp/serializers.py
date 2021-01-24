cad1c17feb561d5ed6a1b5317ca973c3538ba500
kuboit217 <viet.dinh217@gmail.com>
2021-01-24 10:55:20 +0700

    Relational Model Rest API | CompanyBank Views

--- DjangoMedicalApp/serializers.py
+++ DjangoMedicalApp/serializers.py
@@ -1,8 +1,20 @@
 from rest_framework import serializers
-from DjangoMedicalApp.models import Company
+from DjangoMedicalApp.models import Company , CompanyBank
 
-class CompanySerliazer(serializers.HyperlinkedModelSerializer):
+#tạo lớp Company
+class CompanySerliazer(serializers.ModelSerializer):
     class Meta:
         model = Company
-        fields = ['name','license_no','address','contact_no','email','description']
-        
+        fields = '__all__'
+
+#tạo lớp company bank
+class CompanyBankSerliazer(serializers.ModelSerializer):
+    class Meta:
+        model = CompanyBank
+        fields = '__all__'
+
+    #tạo khóa ngoại lấy info compnay
+    def to_representation(self, instance):
+        response = super().to_representation(instance)
+        response['company'] = CompanySerliazer(instance.company_id).data
+        return response
--- DjangoMedicalApp/views.py
+++ DjangoMedicalApp/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-from rest_framework import viewsets
-
+from rest_framework import viewsets, generics
 
 
 # Create your views here.
@@ -9,8 +8,9 @@ from rest_framework.generics import get_object_or_404
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.permissions import IsAuthenticated
 
-from DjangoMedicalApp.models  import Company
-from DjangoMedicalApp.serializers import CompanySerliazer
+
+from DjangoMedicalApp.models  import Company , CompanyBank
+from DjangoMedicalApp.serializers import CompanySerliazer , CompanyBankSerliazer
 
 #viewset company
 class CompanyViewSet(viewsets.ViewSet):
@@ -29,7 +29,7 @@ class CompanyViewSet(viewsets.ViewSet):
     def create(self,request):
         try:
             serializer = CompanySerliazer(data = request.data,context={'request':request})
-            serializer.is_valid()
+            serializer.is_valid(raise_exception=True)
             serializer.save() 
             dict_reponse = {'error':False, 'message':'Company Data Save Susscesfully'}
         except:
@@ -47,7 +47,64 @@ class CompanyViewSet(viewsets.ViewSet):
             dict_reponse = {'error':False, 'message':'Susscesfully Update Company Data'}   
         except:
             dict_reponse = {'error':False, 'message':'Error During Update Company Data'}    
-        return Response(dict_reponse)        
+        return Response(dict_reponse)  
+
+#viewset CompanyBank.
+class CompanyBankViewSet(viewsets.ViewSet): 
+    #tạo và kiểm tra token
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    #tạo hàm create company bank
+    def create(self,request):
+        try:
+            serializer = CompanyBankSerliazer(data = request.data,context={'request':request})
+            serializer.is_valid(raise_exception=True)
+            serializer.save() 
+            dict_reponse = {'error':False, 'message':'Company Bank Data Save Susscesfully'}
+        except:
+            dict_reponse = {'error':True, 'message':'Error During Saving Company Bank Data'} 
+        return Response(dict_reponse)  
+    
+    #tạo hàm list company bank
+    def list(self,request):
+        compnaybank = CompanyBank.objects.all()
+        serializer = CompanyBankSerliazer(compnaybank, many=True, context={'request':request}) 
+        reponse_dict = {'error':False, 'message':'All Company Bank List Data','data':serializer.data}
+        return Response(reponse_dict)
+
+    #hàm lấy 1 thông tin vào data trong CompanyBank
+    def retrieve(self,request,pk=None):
+        queryset = CompanyBank.objects.all()
+        companybank = get_object_or_404(queryset, pk = pk)
+        serializer = CompanyBankSerliazer(companybank,context={'request':request})
+        reponse_dict = {'error':False, 'message':'Single Data Fetch','data':serializer.data}
+        return Response(reponse_dict)
+
+    #hàm update compnay bank 
+    def update(self,request,pk=None):
+        try:
+            queryset = CompanyBank.objects.all()
+            companybank = get_object_or_404(queryset, pk = pk)
+            serializer = CompanyBankSerliazer(companybank,data = request.data,context={'request':request})
+            serializer.is_valid()
+            serializer.save()
+            dict_reponse = {'error':False, 'message':'Data has Been Update'}
+        except:
+            dict_reponse = {'error':True, 'message':'Data Company Bank Cannot Update'}
+        return Response(dict_reponse)
+
+#class xem tên bank
+class CompanyNameViewset(generics.ListAPIView):
+    serializer_class = CompanySerliazer
+    def get_queryset(self):
+        name = self.kwargs['name']
+        return Company.objects.filter(name=name)
+
+
+
+
+
 
 company_list = CompanyViewSet.as_view({'get':'list'})
 company_create = CompanyViewSet.as_view({'post':'create'})
